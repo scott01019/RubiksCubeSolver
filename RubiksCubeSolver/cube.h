a62dbb76c10d5df5aaaa7b463bbced41aa45e567
scott01019 <scott01019@gmail.com>
2017-02-09 03:18:01 -0500

    Added Cube Header File
    Added the header file for the Cube class.

---
+++ RubiksCubeSolver/cube.h
@@ -0,0 +1,73 @@
+#ifndef CUBE_H_
+#define CUBE_H_
+
+#include "cubie.h"
+
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+/*
+ A class representing a generic Cube.
+*/
+class Cube {
+public:
+  static const std::string MOVES[18];
+  static const std::unordered_map<std::string, void(Cube::*)(void)> MOVES_MAP; // Move -> Move Method
+  static const std::unordered_map<std::tuple<char, char>, char> ROTATION_MAP; // Current Face and Axis -> new Face
+  static const int SIZE = 3;
+
+  Cube() { init(); }
+
+  void Move(char, int, int);
+  void Move(const std::string &);
+  void Move(const std::vector<std::string> &);
+  void Rotate(char, Cubie &);
+  void RotateX(Cubie &);
+  void RotateY(Cubie &);
+  void RotateZ(Cubie &);
+  const Cubie &GetCubieByPosition(const std::tuple<int, int, int> &) const;
+  const std::vector<Cubie &> &GetUnsolvedCorners() const;
+  const std::vector<Cubie &> &GetUnsolvedCubies() const;
+  const std::vector<Cubie &> &GetUnsolvedEdges() const;
+  const std::vector<std::string> GetT() const;
+  void Scramble();
+  bool Solved() const;
+
+  // Move Methods
+  void R() { Cube::Move('x', 1, 1); }
+  void Ri() { Cube::Move('x', 1, 3); }
+  void R2() { Cube::Move('x', 1, 2); }
+
+  void L() { Cube::Move('x', -1, 3); }
+  void Li() { Cube::Move('x', -1, 1); }
+  void L2() { Cube::Move('x', -1, 2); }
+
+  void F() { Cube::Move('z', -1, 3); }
+  void Fi() { Cube::Move('z', -1, 1); }
+  void F2() { Cube::Move('z', -1, 2); }
+
+  void B() { Cube::Move('z', 1, 1); }
+  void Bi() { Cube::Move('z', 1, 3); }
+  void B2() { Cube::Move('z', 1, 2); }
+
+  void U() { Cube::Move('y', 1, 1); }
+  void Ui() { Cube::Move('y', 1, 3); }
+  void U2() { Cube::Move('y', 1, 2); }
+
+  void D() { Cube::Move('y', -1, 3); }
+  void Di() { Cube::Move('y', -1, 1); }
+  void D2() { Cube::Move('y', -1, 2); }
+
+  void T();
+
+  const std::vector<Cubie &> &cubies() const { return cubies_; }
+  int size() const { return SIZE; }
+private:
+  void init();
+
+  std::vector<Cubie &> cubies_; // A vector of the cubies.
+};
+
+#endif // CUBE_H_
