45f270a045f13fd9eaf3e2f31c411139c7df5a96
scott01019 <scott01019@gmail.com>
2017-02-09 01:18:53 -0500

    Added Cubie.h
    Added header file for the Cubie class

---
+++ RubiksCubeSolver/cubie.h
@@ -0,0 +1,35 @@
+#ifndef CUBIE_H_
+#define CUBIE_H_
+
+#include <utility>
+#include <vector>
+
+/*
+  A class representing a cubie (edge, center, or corner) of the cube.
+*/
+class Cubie {
+public:
+  Cubie(const std::tuple<int, int, int> &);
+  Cubie(const Cubie &);
+
+  bool IsCorner() const;
+  bool IsEdge() const;
+  bool IsCenter() const;
+  bool IsSolved() const;
+  bool IsOriented() const;
+  bool IsInAxisLayer(char, int) const;
+
+  const std::tuple<int, int, int> &position() const { return position_; }
+  const std::tuple<int, int, int> &solved_position() const { return solved_position_; }
+  const std::vector<std::tuple<char, char, char>> &values() const { return values_;  }
+
+  void set_position(const std::tuple<int, int, int> &pos) { position_ = pos; }
+  void SetFaceForValue(char value, char face);
+
+private:
+  std::tuple<int, int, int> positon_; // x, y, z
+  const std::tuple<int, int, int> solved_position_; // x, y, z
+  std::vector<std::tuple<char, char, char>> values_; // value, face value is currently on, face value is solved on
+}
+
+#endif // CUBIE_H_
